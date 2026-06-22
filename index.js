bfe2b45b7fd723375b2a151eb112af4f37389452
contatoeufelipenogueira <contato.eufelipenogueira@gmail.com>
2026-06-21 23:39:54 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import { startWhatsApp } from './whatsapp.js';
-
-console.log('🚀 Iniciando agente de IA para WhatsApp (modo teste local)...');
-
-startWhatsApp().catch((err) => {
-  console.error('Erro fatal ao iniciar:', err);
-  process.exit(1);
-});
